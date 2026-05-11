949186864c22e31c742cc771687a714caf7bdbd4
nandeshwar7678 <akashya7392@gmail.com>
2026-05-11 18:28:18 +0530

    AWPL project processing

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,11 @@ app.use(express.json());
 // MONGODB CONNECT
 // ======================
 
+// mongoose.connect(
+//     "mongodb://DeepakBhai1:Akashya12@ac-srheovm-shard-00-00.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-01.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-02.n73gk0u.mongodb.net:27017/?ssl=true&replicaSet=atlas-w9rfv9-shard-0&authSource=admin&appName=Cluster0"
+// )
 mongoose.connect(
-    // "mongodb://DeepakBhai1:Akashya12@ac-srheovm-shard-00-00.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-01.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-02.n73gk0u.mongodb.net:27017/?ssl=true&replicaSet=atlas-w9rfv9-shard-0&authSource=admin&appName=Cluster0"
-    mongoose.connect("mongodb+srv://DeepakBhai1:Akashya12@cluster0.n73gk0u.mongodb.net/contactDB?retryWrites=true&w=majority")
+"mongodb://DeepakBhai1:Akashya12@ac-srheovm-shard-00-00.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-01.n73gk0u.mongodb.net:27017,ac-srheovm-shard-00-02.n73gk0u.mongodb.net:27017/contacts?ssl=true&replicaSet=atlas-w9rfv9-shard-0&authSource=admin&retryWrites=true&w=majority"
 )
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
@@ -115,7 +117,6 @@ app.post("/contact", async (req, res) => {
 
 
 
-
 // ======================
 // START SERVER
 // ======================
@@ -127,5 +128,5 @@ app.post("/contact", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
+    console.log(`Server Running on ${PORT}`);
 });
